d78f852a941a93388ae30acfdd305042431d1d62
Silvia Rodriguez <silviarodriguez@silvias-air.whitworth.edu>
2018-02-28 20:16:28 -0800

    Initial Commit

---
+++ Muzik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muzik
+//
+//  Created by Silvia Rodriguez on 2/28/18.
+//  Copyright © 2018 Silvia Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
